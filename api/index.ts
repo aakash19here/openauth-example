bfbe89624e90530a06a6797501f6701f283c04a7
Aakash <aakash19here@gmail.com>
2025-04-06 17:49:38 +0530

    refactor(api): use hono node server

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,8 @@
-/// <reference types="bun-types" />
-
 import { createClient } from "@openauthjs/openauth/client"
 import { subjects } from "../auth/subjects"
+import { Hono } from "hono"
+import { serve } from '@hono/node-server'
+import { cors } from "hono/cors"
 
 const headers = {
   "Access-Control-Allow-Origin": "*",
@@ -14,34 +15,49 @@ const client = createClient({
   issuer: "http://localhost:3000",
 })
 
-const server = Bun.serve({
-  port: 3001,
-  async fetch(req: Request) {
-    const url = new URL(req.url)
+const app = new Hono()
 
-    if (req.method === "OPTIONS") {
-      return new Response(null, { headers })
+app.use("*", cors({
+  origin: ["http://localhost:3000", "http://localhost:5173"],
+  allowHeaders: [...Object.keys(headers), "Authorization", "Content-Type"],
+  allowMethods: ["GET", "POST", "PATCH", "OPTIONS"],
+  credentials: true
+}))
+
+app.get(async(c) => {
+  const url = new URL(c.req.url)
+
+  if (url.pathname === "/") {
+    const authHeader = c.req.header("Authorization")
+
+    if (!authHeader) {
+      return new Response("Unauthorized: No authorization header", { 
+        headers, 
+        status: 401 
+      })
     }
 
-    if (url.pathname === "/" && req.method === "GET") {
-      const authHeader = req.headers.get("Authorization")
+    const token = authHeader.split(" ")[1]
+    const verified = await client.verify(subjects, token)
 
-      if (!authHeader) {
-        return new Response("401", { headers, status: 401 })
-      }
-
-      const token = authHeader.split(" ")[1]
-      const verified = await client.verify(subjects, token)
-
-      if (verified.err) {
-        return new Response("401", { headers, status: 401 })
-      }
-
-      return new Response(verified.subject.properties.userID, { headers })
+    if (verified.err) {
+      return new Response("Unauthorized: Invalid token", { 
+        headers, 
+        status: 401 
+      })
     }
 
-    return new Response("404", { status: 404 })
-  },
+    return new Response(verified.subject.properties.userID, { headers })
+
+  }
+
+  return new Response("404", { status: 404 })
+
 })
 
-console.log(`Listening on ${server.port}`)
+serve({
+  fetch: app.fetch,
+  port: 3001,
+})
+
+
